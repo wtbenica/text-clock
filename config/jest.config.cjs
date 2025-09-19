256280522b325e43735fba3a6374efb2cb61ccec
wesley <wesley@benica.dev>
2025-09-18 22:24:52 -0700

    chore: remove root duplicates, update .gitgnore and fix jest rootDir

---
+++ config/jest.config.cjs
@@ -0,0 +1,39 @@
+module.exports = {
+    preset: 'ts-jest/presets/default-esm',
+    // Ensure <rootDir> points to the repository root (config/ is the config location)
+    rootDir: '..',
+    extensionsToTreatAsEsm: ['.ts'],
+    testEnvironment: 'node',
+    roots: ['<rootDir>/tests', '<rootDir>'],
+    testMatch: ['**/tests/**/*.test.ts', '**/?(*.)+(spec|test).ts'],
+    transform: {
+        '^.+\\.ts$': ['ts-jest', {
+            useESM: true,
+            diagnostics: {
+                warnOnly: true,
+                ignoreCodes: [151001]
+            }
+        }],
+    },
+    moduleFileExtensions: ['ts', 'js', 'json'],
+    collectCoverageFrom: [
+        '**/*.ts',
+        '!**/*.d.ts',
+        '!**/node_modules/**',
+        '!**/tests/**',
+        '!**/dist/**',
+        '!**/config/**',
+        '!**/constants/**/index.ts',
+        '!**/constants/**/extension.ts',
+        '!**/constants/**/prefs.ts',
+        '!extension.ts',
+        '!ui/**',
+    ],
+    coverageDirectory: '<rootDir>/coverage',
+    coverageReporters: ['text', 'lcov', 'html'],
+    setupFilesAfterEnv: ['<rootDir>/tests/setup.ts'],
+    moduleNameMapper: {
+        '^(\\.{1,2}/.*)\\.js$': '$1',
+    },
+    testTimeout: 10000,
+};
